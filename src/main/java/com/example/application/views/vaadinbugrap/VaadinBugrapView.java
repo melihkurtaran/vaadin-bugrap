c8543a40695eb0f2db00f57317ebf50de22c10ad
Melih Kurtaran <melihkurtaran@sabanciuniv.edu>
2022-02-09 09:56:16 +0200

    Route added for About

--- src/main/java/com/example/application/views/vaadinbugrap/VaadinBugrapView.java
+++ src/main/java/com/example/application/views/vaadinbugrap/VaadinBugrapView.java
@@ -1,5 +1,8 @@
 package com.example.application.views.vaadinbugrap;
 
+import com.example.application.views.about.AboutView;
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.HasComponents;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Paragraph;
@@ -7,13 +10,15 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
+import com.vaadin.flow.router.RouterLink;
+
 import javax.annotation.security.RolesAllowed;
 
 @PageTitle("Vaadin Bugrap")
 @Route(value = "vaadin")
 @RouteAlias(value = "")
 @RolesAllowed("user")
-public class VaadinBugrapView extends VerticalLayout {
+public class VaadinBugrapView extends VerticalLayout{
 
     public VaadinBugrapView() {
         setSpacing(false);
@@ -22,13 +27,16 @@ public class VaadinBugrapView extends VerticalLayout {
         img.setWidth("200px");
         add(img);
 
-        add(new H2("This place intentionally left empty"));
-        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+        add(new H2("Melih's Bugrap Project will be here soon!"));
+        add(new Paragraph("Wait for it! \uD83D\uDE0E"));
 
         setSizeFull();
         setJustifyContentMode(JustifyContentMode.CENTER);
         setDefaultHorizontalComponentAlignment(Alignment.CENTER);
         getStyle().set("text-align", "center");
+
+        add(new RouterLink("About", AboutView.class));
+        add(new RouterLink("Vaadin", VaadinBugrapView.class));
     }
 
 }
