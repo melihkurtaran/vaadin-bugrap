fb589b377a9e8b521b97464c64df5b1f97546668
Melih Kurtaran <melihkurtaran@sabanciuniv.edu>
2022-02-09 11:26:48 +0200

    login auth attempt

--- src/main/java/com/example/application/views/vaadinbugrap/VaadinBugrapView.java
+++ src/main/java/com/example/application/views/vaadinbugrap/VaadinBugrapView.java
@@ -1,16 +1,18 @@
 package com.example.application.views.vaadinbugrap;
 
 import com.example.application.views.about.AboutView;
+import com.example.application.views.login.LoginView;
 import com.vaadin.flow.component.Composite;
 import com.vaadin.flow.component.HasComponents;
+import com.vaadin.flow.component.HasElement;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.login.AbstractLogin;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.router.PageTitle;
-import com.vaadin.flow.router.Route;
-import com.vaadin.flow.router.RouteAlias;
-import com.vaadin.flow.router.RouterLink;
+import com.vaadin.flow.router.*;
+import com.vaadin.flow.server.VaadinSession;
 
 import javax.annotation.security.RolesAllowed;
 
@@ -22,6 +24,12 @@ public class VaadinBugrapView extends VerticalLayout{
 
     public VaadinBugrapView() {
         setSpacing(false);
+
+        HorizontalLayout MenuBar = new HorizontalLayout();
+        MenuBar.setWidth("20%");
+        MenuBar.add(new RouterLink("About", AboutView.class));
+        MenuBar.add(new RouterLink("Vaadin", VaadinBugrapView.class));
+        add(MenuBar);
 
         Image img = new Image("images/empty-plant.png", "placeholder plant");
         img.setWidth("200px");
@@ -35,8 +43,18 @@ public class VaadinBugrapView extends VerticalLayout{
         setDefaultHorizontalComponentAlignment(Alignment.CENTER);
         getStyle().set("text-align", "center");
 
-        add(new RouterLink("About", AboutView.class));
-        add(new RouterLink("Vaadin", VaadinBugrapView.class));
+
     }
+    /*
+    @Override
+    public void beforeEnter(BeforeEnterEvent event)
+    {
+        if(  VaadinSession.getCurrent().getAttribute("userLoggedIn") == null) {
+            VaadinSession.getCurrent().setAttribute("intendedPath",event.getLocation().getPath());
+            event.rerouteTo(LoginView.class);
+        }
+    }
+
+     */
 
 }
