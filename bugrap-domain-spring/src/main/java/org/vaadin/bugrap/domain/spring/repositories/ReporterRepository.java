3574d572a5746155591964fd5bdf8e17e043f139
Melih Kurtaran <melihkurtaran@sabanciuniv.edu>
2022-03-08 12:40:17 +0200

    delette non-nec. classes

--- bugrap-domain-spring/src/main/java/org/vaadin/bugrap/domain/spring/repositories/ReporterRepository.java
+++
@@ -1,9 +0,0 @@
-package org.vaadin.bugrap.domain.spring.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.vaadin.bugrap.domain.entities.Reporter;
-
-public interface ReporterRepository extends JpaRepository<Reporter, Long> {
-    org.vaadin.bugrap.domain.entities.Reporter getByNameAndPassword(String name, String password);
-    org.vaadin.bugrap.domain.entities.Reporter getByNameOrEmail(String name, String email);
-}
